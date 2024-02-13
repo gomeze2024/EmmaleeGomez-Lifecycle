bf8ef463cb7e41a8bca755d4e6c944d8bc1ddf3a
Emmalee Gomez <gomeze@bu.edu>
2024-02-13 16:02:13 -0500

    implement blank activity that logs activity lifecycle

---
+++ app/src/main/java/com/example/emmaleegomez_lifecycle/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.emmaleegomez_lifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy() called")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
