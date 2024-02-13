0ee26777cad97dcfb47863beb3e11f0e329e7570
Emmalee Gomez <gomeze@bu.edu>
2024-02-13 17:02:08 -0500

    add button that changes text view and edit view to Hello

--- app/src/main/java/com/example/emmaleegomez_lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/emmaleegomez_lifecycle/MainActivity.kt
@@ -3,11 +3,29 @@ package com.example.emmaleegomez_lifecycle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button : Button
+    private lateinit var textView: TextView
+    private lateinit var editText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "onCreate() called")
+
+        button = findViewById(R.id.button)
+        textView = findViewById(R.id.text_view)
+        editText = findViewById(R.id.edit_text)
+
+        button.setOnClickListener {view: View ->
+            textView.text = getString(R.string.hello)
+            editText.setText(getString(R.string.hello))
+        }
+
     }
 
     override fun onStart() {
